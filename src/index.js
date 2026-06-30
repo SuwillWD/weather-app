d1c5c88e05434bc1eccc80990a94a031590e2b25
SuwillWD <136084114+SuwillWD@users.noreply.github.com>
2026-06-30 15:56:41 +0530

    Add get & process weather data

--- src/index.js
+++ src/index.js
@@ -1,5 +1,68 @@
 import './styles.css';
 
-for (let i = 0; i < 10; i++) {
-  console.log(i);
+async function getWeatherData(location) {
+  let url =
+    'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' +
+    location +
+    '?key=WSWAVFESEW8P8EA8PB7QBTRCH';
+  let response = await fetch(url);
+  let data = await response.json();
+  return data;
 }
+
+async function processWeatherData(location) {
+  let rawData = await getWeatherData(location);
+
+  function pick(obj, keys) {
+    return Object.fromEntries(keys.map((key) => [key, obj[key]]));
+  }
+
+  let currentConditions = pick(rawData.currentConditions, [
+    'conditions',
+    'datetime',
+    'humidity',
+    'icon',
+    'pressure',
+    'sunrise',
+    'sunset',
+    'temp',
+    'visibility',
+    'windspeed',
+  ]);
+
+  let sevenDaysForcast = [];
+
+  for (let i = 0; i < 7; i++) {
+    sevenDaysForcast.push(
+      pick(rawData.days[i], [
+        'conditions',
+        'datetime',
+        'humidity',
+        'icon',
+        'pressure',
+        'sunrise',
+        'sunset',
+        'temp',
+        'tempmax',
+        'tempmin',
+        'visibility',
+        'windspeed',
+      ])
+    );
+  }
+
+  const description = rawData.description;
+  const timezone = rawData.timezone;
+
+  return {
+    location,
+    currentConditions,
+    sevenDaysForcast,
+    description,
+    timezone,
+  };
+}
+
+processWeatherData('nagpur').then((response) => {
+  console.log(response.sevenDaysForcast);
+});
