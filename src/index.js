7e9ef484c563beb7c89849728a67fd1f1d4bd833
SuwillWD <136084114+SuwillWD@users.noreply.github.com>
2026-06-30 17:39:29 +0530

    Add handle user requests

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
 import './styles.css';
 
 async function getWeatherData(location) {
-  let url =
-    'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' +
-    location +
-    '?key=WSWAVFESEW8P8EA8PB7QBTRCH';
-  let response = await fetch(url);
-  let data = await response.json();
-  return data;
+  try {
+    let url =
+      'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' +
+      location +
+      '?key=WSWAVFESEW8P8EA8PB7QBTRCH';
+    let response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Invalid Request!`);
+    }
+    let data = await response.json();
+    return data;
+  } catch (error) {
+    return error;
+  }
 }
 
 async function processWeatherData(location) {
   let rawData = await getWeatherData(location);
+
+  if (rawData instanceof Error) {
+    // Todo - handle invalid request functions
+    return;
+  }
 
   function pick(obj, keys) {
     return Object.fromEntries(keys.map((key) => [key, obj[key]]));
@@ -63,6 +76,29 @@ async function processWeatherData(location) {
   };
 }
 
-processWeatherData('nagpur').then((response) => {
-  console.log(response.sevenDaysForcast);
-});
+function validateForm(form) {
+  let inputValue = form.children[0];
+  if (inputValue.validity.valueMissing) {
+    inputValue.setCustomValidity('Please enter a location.');
+    inputValue.reportValidity();
+    return false;
+  } else {
+    inputValue.setCustomValidity('');
+    return true;
+  }
+}
+
+const handleUserRequests = (() => {
+  const homeForm = document.getElementById('home-form');
+
+  homeForm.addEventListener('submit', async (event) => {
+    event.preventDefault();
+    if (!validateForm(homeForm)) {
+      return;
+    }
+    const searchValue = document.getElementById('home-input').value;
+    let newData = await processWeatherData(searchValue);
+    console.log(newData.description);
+    homeForm.reset();
+  });
+})();
--- webpack.common.js
+++ webpack.common.js
@@ -18,6 +18,10 @@ export default {
         test: /\.css$/i,
         use: ['style-loader', 'css-loader'],
       },
+      {
+        test: /\.html$/i,
+        use: ['html-loader'],
+      },
     ],
   },
   plugins: [
